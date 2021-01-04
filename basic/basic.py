c4feb996fece7d24b6091cb54c596eec799931b0
nedate12 <edate.nitin@gmail.com>
2021-01-04 10:22:17 +0530

    committed basic file

---
+++ basic/basic.py
@@ -0,0 +1,24 @@
+import math as m
+
+# normal number calculation
+i = 10
+j = 2
+k = 11
+print(i + j)
+print(i - j)
+print(i * j)
+print(i / j)
+print(i // j)
+print(k % j)
+
+# Advance number
+a = 12
+print(bin(a))
+print(hex(a))
+
+print(m.floor(3.7))
+print(m.ceil(3.7))
+print(m.sqrt(9))
+
+print('Type of a :', type(a))
+
